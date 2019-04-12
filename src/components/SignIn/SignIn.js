6db5cb56cbf82723920eae5a55a5fea87a3970bc
Shlomi Ohana <snoop.ohana@gmail.com>
2019-04-12 19:35:09 +0300

    deploy

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('https://enigmatic-savannah-41616.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
